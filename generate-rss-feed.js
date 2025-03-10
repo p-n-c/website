4463660118436de4ed0ee60367ae9d1c3da44d6b
Daniel Hartley <daniel.hartley@altruistiq.com>
2025-03-10 07:22:36 +0000

    feat: update rss generator args

--- generate-rss-feed.js
+++ generate-rss-feed.js
@@ -4,18 +4,18 @@ const __dirname = import.meta.dirname
 import { rssFeeder } from '@p-n-c/rssfeed'
 
 const src = path.join(__dirname, 'src')
+const root = 'src'
+const feed = {
+  title: 'People and Code, At Your Disposal',
+  link: 'https://people-and-code.com/',
+  description: 'Latest articles from People and Code',
+  outputPath: 'feed.rss',
+}
 const options = {
-  root: 'src',
   images: {
     main: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Beardsley-peacockskirt.PNG/430px-Beardsley-peacockskirt.PNG',
   },
   pathsToExclude: ['/'],
-  feed: {
-    title: 'People and Code, At Your Disposal',
-    link: 'https://people-and-code.com/',
-    description: 'Latest articles from People and Code',
-    outputPath: 'feed.rss',
-  },
 }
 
-rssFeeder.generateRssFeed(src, options)
+rssFeeder.generateRssFeed(src, root, feed, options)
